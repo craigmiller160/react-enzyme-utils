a8724da0b595b42046f427166cc08ed24d4ac030
Craig Miller <craigmiller160@gmail.com>
2020-03-16 18:11:56 -0400

    Added createTestReduxProvider

--- src/createTestReduxProvider.tsx
+++ src/createTestReduxProvider.tsx
@@ -1,41 +1,32 @@
-import React from 'react';
+import React, { ReactNode, FC } from 'react';
+import createMockStore, { MockStoreCreator, MockStoreEnhanced } from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { Middleware } from 'redux';
 
-const createProvider = (storeState, useThunk) => {
-    const { Provider } = require('react-redux');
-    const configureMockStore = require('redux-mock-store').default;
+interface Props {
+    children: ReactNode
+}
 
-    let middleware = [];
+const createTestReduxProvider = (reduxState: object, useThunk: boolean): [ReactNode,MockStoreEnhanced<typeof reduxState,any>] => {
+    const middleware: Array<Middleware> = [];
     if (useThunk) {
-        const thunk = require('redux-thunk').default;
         middleware.push(thunk);
     }
 
-    const mockStore = configureMockStore(middleware);
-    const store = mockStore(storeState);
+    const mockStore: MockStoreCreator<typeof reduxState,any> = createMockStore(middleware);
+    const store: MockStoreEnhanced<typeof reduxState,any> = mockStore(reduxState);
 
-    const ReduxProvider = (props) => (
-        <Provider store={ store }>
-            { props.children }
-        </Provider>
-    );
+    const ReduxProvider: FC<Props> = (props: Props) => {
+        const { children } = props;
+        return (
+            <Provider store={ store }>
+                { children }
+            </Provider>
+        );
+    };
 
     return [ReduxProvider, store];
-};
-
-const createTestReduxProvider = async (reduxState: object, useThunk: boolean) => {
-    const { Provider } = await import('react-redux');
-    const configureMockStore = await import('redux-mock-store');
-
-    const middleware: Array<any> = []; // TODO what type for the array?
-    if (useThunk) {
-        const thunk = await import('redux-thunk');
-        middleware.push(thunk);
-    }
-
-    const mockStore = configureMockStore(middleware); // TODO why is this not working?
-    const store = mockStore(reduxState);
-
-    // TODO finish this
 };
 
 export default createTestReduxProvider;
