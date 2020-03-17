3dd3c42c58566c81da6b02b81522b8211ca97be0
Craig Miller <craigmiller160@gmail.com>
2020-03-17 16:00:17 -0400

    Improved types in createTestReduxProvider.

--- src/createTestReduxProvider.tsx
+++ src/createTestReduxProvider.tsx
@@ -4,15 +4,14 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { Middleware } from 'redux';
 
-const createTestReduxProvider = (reduxState: object, useThunk: boolean): // TODO need generic type
-    [FC<PassThroughCompProps>, MockStoreEnhanced<typeof reduxState, object>] => {
+function createTestReduxProvider<State = {}>(reduxState: State, useThunk: boolean): [FC<PassThroughCompProps>, MockStoreEnhanced<State, object>] {
     const middleware: Array<Middleware> = [];
     if (useThunk) {
         middleware.push(thunk);
     }
 
-    const mockStore: MockStoreCreator<typeof reduxState, object> = createMockStore(middleware);
-    const store: MockStoreEnhanced<typeof reduxState, object> = mockStore(reduxState);
+    const mockStore: MockStoreCreator<State, object> = createMockStore(middleware);
+    const store: MockStoreEnhanced<State, object> = mockStore(reduxState);
 
     const ReduxProvider: FC<PassThroughCompProps> = (props: PassThroughCompProps) => {
         const { children } = props;
@@ -24,6 +23,6 @@ const createTestReduxProvider = (reduxState: object, useThunk: boolean): // TODO
     };
 
     return [ ReduxProvider, store ];
-};
+}
 
 export default createTestReduxProvider;
